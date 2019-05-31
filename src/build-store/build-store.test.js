3c972b352438586aa5081e58ee98529b6cd71340
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-01 09:25:18 +1000

    Add mock logger for build-store.test

--- src/build-store/build-store.test.js
+++ src/build-store/build-store.test.js
@@ -1,26 +1,28 @@
 const { InMemoryCreatorStore } = require('./build-store');
 
+const mockLog = { log: { info: jest.fn() } };
+
 describe('InMemoryBuildStore', () => {
   test('should put and get build', () => {
-    const store = InMemoryCreatorStore();
+    const store = InMemoryCreatorStore(mockLog);
     store.put({ id: 'hello', name: 'world' });
     expect(store.get('hello')).toEqual({ id: 'hello', name: 'world' });
   });
 
   test('should return undefined if no build for id', () => {
-    const store = InMemoryCreatorStore();
+    const store = InMemoryCreatorStore(mockLog);
     expect(store.get('nonexistent')).toBeUndefined();
   });
 
   test('should delete builds too', () => {
-    const store = InMemoryCreatorStore();
+    const store = InMemoryCreatorStore(mockLog);
     store.put({ id: 'sup', name: 'dilip' });
     store.delete('sup');
     expect(store.get('sup')).toBeUndefined();
   });
 
   test('should throw Error if no id present', (done) => {
-    const store = InMemoryCreatorStore();
+    const store = InMemoryCreatorStore(mockLog);
     try {
       store.put({ hello: 'world' });
       done.fail('should have thrown Error');
