951e907d6a3b900b55dccd9fac843f80d86fd4cd
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-02 23:36:32 +1000

    Allow koa cors.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Bunyan = require('bunyan');
 const http = require('http');
+const cors = require('@koa/cors');
 const { InMemorySessionStore } = require('./session-store');
 const { InMemoryCreatorStore } = require('./build-store');
 const { CreatorRouter } = require('./healthcheck-create');
@@ -15,6 +16,7 @@ const sessionRouter = SessionRouter({ sessionStore, log });
 const app = new Koa();
 app.use(creatorRouter.routes());
 app.use(sessionRouter.routes());
+app.use(cors());
 
 const httpServer = http.createServer(app.callback());
 const sessionManager = SessionManager({ httpServer, sessionStore, log });
