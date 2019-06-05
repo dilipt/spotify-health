53efb4517da6b22a64cea6e35d84509765b9821e
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-06-05 23:43:02 +1000

    Remove unnecessary logging.

--- src/healthcheck-session/socket-manager.js
+++ src/healthcheck-session/socket-manager.js
@@ -9,15 +9,9 @@ const init = ({ httpServer, logger }) => {
 };
 
 const createNamespace = (name) => {
-  log.info(`Creating namespace for ${name}`);
   const nsp = io.of(`/sessions/${name}`);
   nsp.on('connection', (socket) => {
-    log.info(`Someone has joined ${name}`);
-
-    nsp.emit('hi', 'hello');
-
     socket.on('vote placed', (data) => {
-      log.info(`Incoming Vote: ${JSON.stringify(data)}`);
       nsp.emit('vote accepted', data);
     });
   });
